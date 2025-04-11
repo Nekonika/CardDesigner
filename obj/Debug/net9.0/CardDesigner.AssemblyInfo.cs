bd388025b649fa41543413f372e03214dc24650f
Nekonika <51423438+Nekonika@users.noreply.github.com>
2025-04-11 14:56:58 +0200

    Update CardDesigner.AssemblyInfo.cs

--- obj/Debug/net9.0/CardDesigner.AssemblyInfo.cs
+++ obj/Debug/net9.0/CardDesigner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardDesigner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c95dd480da2cbea8cf56b931635a5847b046465")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardDesigner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardDesigner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
